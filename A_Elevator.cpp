f57fe5ff10cb91496aeef82e5c4579afc95abb85
Pallab <mazharulislampallab98@gmail.com>
2026-06-21 20:56:20 +0600

    codeforces problem solve 2 21 june 2026

---
+++ A_Elevator.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    string door;
+    int rail;
+
+    cin >> door >> rail;
+
+    if (door == "front")
+    {
+        if (rail == 1)
+            cout << "L";
+        else
+            cout << "R";
+    }
+    else
+    {
+        if (rail == 1)
+            cout << "R";
+        else
+            cout << "L";
+    }
+
+    return 0;
+}
