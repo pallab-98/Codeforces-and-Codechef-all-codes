223490ac6c7e1cf9711fb55812db6fc19827fab9
Pallab <mazharulislampallab98@gmail.com>
2026-06-23 20:04:57 +0600

    Codeforces problem solve 2 Tuesday 23 june 26

---
+++ A_Short_Substrings.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int caseTest;
+    cin >> caseTest;
+
+    while (caseTest--) {
+        string b;
+        cin >> b;
+
+        string ans = "";
+        ans += b[0];
+        ans += b[1];
+
+        for (int i = 3; i < b.size(); i += 2) {
+            ans += b[i];
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
