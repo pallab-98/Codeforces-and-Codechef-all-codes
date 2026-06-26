261a2e52318023a6f69d71f8339721c7e3998463
Pallab <mazharulislampallab98@gmail.com>
2026-06-26 22:56:22 +0600

    update

---
+++ A_Robot_Program.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+
+        if (x == 0 && y == 0) {
+            cout << 0 << '\n';
+            continue;
+        }
+
+        int mx = max(x, y);
+        int mn = min(x, y);
+
+        if (mx == mn)
+            cout << 2 * mx << '\n';
+        else if (mx == mn + 1)
+            cout << 2 * mx - 1 << '\n';
+        else
+            cout << 4 * mx - 2 * mn - 3 << '\n';
+    }
+
+    return 0;
+}
