49c83890e989272389a1c998598017a4673a7fab
Pallab <mazharulislampallab98@gmail.com>
2026-06-26 23:32:45 +0600

    update

--- A_Three_Pairwise_Maximums.cpp
+++ A_Three_Pairwise_Maximums.cpp
@@ -15,9 +15,8 @@ int main()
         sort(arr, arr + 3);
 
         if (arr[1] != arr[2])
-        {
+
             cout << "NO" << endl;
-        }
         else
         {
             cout << "YES" << endl;
