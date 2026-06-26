7bd493469b2cb660e5c2c2e400844529d806fe90
Pallab <mazharulislampallab98@gmail.com>
2026-06-26 23:00:22 +0600

    update

--- A_Robot_Program.cpp
+++ A_Robot_Program.cpp
@@ -2,28 +2,26 @@
 using namespace std;
 
 int main() {
-    int caseTest;
-    cin >> caseTest;
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
 
     while (t--) {
         int x, y;
         cin >> x >> y;
 
-        if (x == 0 && y == 0) {
-            cout << 0 << '\n';
-            continue;
-        }
-
         int mx = max(x, y);
-        int mn = min(x, y);
 
-        if (mx == mn)
+        if (x == y)
             cout << 2 * mx << '\n';
-        else if (mx == mn + 1)
-            cout << 2 * mx - 1 << '\n';
         else
-            cout << 4 * mx - 2 * mn - 3 << '\n';
+            cout << 2 * mx - 1 << '\n';
     }
 
     return 0;
 }
+
+ 
