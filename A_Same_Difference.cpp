5ff8e000c4cad123a3adc25216f059fe128ee8c4
Pallab <mazharulislampallab98@gmail.com>
2026-06-26 23:38:15 +0600

    update

--- A_Same_Difference.cpp
+++ A_Same_Difference.cpp
@@ -1,42 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     int t;
-    cin>>t;
+    cin >> t;
 
-    while(t--)
+    while (t--)
     {
         int n;
-        cin>>n;
+        cin >> n;
 
         string s;
-        cin>>s;
+        cin >> s;
 
-        int ans=1e9;
+        int ans = 1e9;
 
-        for(char c='a';c<='z';c++)
+        for (char c = 'a'; c <= 'z'; c++)
         {
-            int last=-1;
+            int last = -1;
 
-            for(int i=0;i<n;i++)
-                if(s[i]==c)
-                    last=i;
+            for (int i = 0; i < n; i++)
+                if (s[i] == c)
+                    last = i;
 
-            if(last==-1) continue;
+            if (last == -1)
+                continue;
 
-            int cost=0;
+            int cost = 0;
 
-            for(int i=0;i<last;i++)
-                if(s[i]!=c)
+            for (int i = 0; i < last; i++)
+                if (s[i] != c)
                     cost++;
 
-            cost+=n-last-1;
+            cost += n - last - 1;
 
-            ans=min(ans,cost);
+            ans = min(ans, cost);
         }
 
-        cout<<ans<<"\n";
+        cout << ans << endl;
     }
+    return 0;
 }
