c227482c18c2fa5fc329c397fce00caea60eea83
Pallab <mazharulislampallab98@gmail.com>
2026-06-27 22:12:28 +0600

    Update

---
+++ A_Three_swimmers.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        ll p, a, b, c;
+        cin >> p >> a >> b >> c;
+
+        ll wa = (a - p % a) % a;
+        ll wb = (b - p % b) % b;
+        ll wc = (c - p % c) % c;
+
+        cout << min({wa, wb, wc}) << '\n';
+    }
+
+    return 0;
+}
