2422424795d782fdee7fbce9f7c29d4106945953
Pallab <mazharulislampallab98@gmail.com>
2026-06-29 18:29:54 +0600

    update

--- Hoop_Jump.cpp
+++ Hoop_Jump.cpp
@@ -6,9 +6,10 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    
+
     int caseTime;
     cin >> caseTime;
+    
     while (caseTime--)
     {
         long long a;
