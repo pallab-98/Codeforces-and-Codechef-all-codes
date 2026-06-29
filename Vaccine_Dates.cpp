d969144cfa978a3c0d3a7a69582fa2c06d41fb88
Pallab <mazharulislampallab98@gmail.com>
2026-06-29 18:46:45 +0600

    update

--- Vaccine_Dates.cpp
+++ Vaccine_Dates.cpp
@@ -1,23 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main()
-{   
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    
+
     int caseTest;
     cin >> caseTest;
     while (caseTest--)
     {
-        int a, b, c;
+        long long a, b, c;
         cin >> a >> b >> c;
-        if (a >= b && a < c)
-            cout << "Take second dose now" << endl;
-        else if (a >= b && a > c)
-            cout << "Too Late" << endl;
-        else
+
+        if (a < b)
             cout << "Too Early" << endl;
+
+        else if (a > c)
+            cout << "Too Late" << endl;
+
+        else
+            cout << "Take second dose now" << endl;
     }
 
     return 0;
