4dc8e72e71892f34fbad214b4b2846c5b9bb647f
Pallab <mazharulislampallab98@gmail.com>
2026-06-30 22:00:53 +0600

    Update

---
+++ A_Two_Subsequences.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        char mn = *min_element(s.begin(), s.end());
+
+        int pos = -1;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == mn) {
+                pos = i;
+                break;
+            }
+        }
+
+        cout << s[pos] << " ";
+
+        for (int i = 0; i < s.size(); i++) {
+            if (i != pos)
+                cout << s[i];
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
