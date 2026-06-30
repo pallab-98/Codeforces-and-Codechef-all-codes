2c55171e27cf1f782755834b6dbebcd993d72e62
Pallab <mazharulislampallab98@gmail.com>
2026-06-30 21:21:20 +0600

    Update

---
+++ A_Divide_and_Conquer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cou.ti
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+
+        if (x == y)
+            cout << "YES\n";
+        else if (y > 1 && x % y == 0)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+} 
