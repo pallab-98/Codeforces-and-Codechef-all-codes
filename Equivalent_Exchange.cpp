01a491d6e3ff175264fcb3fa0f6fd568f5157316
Pallab <mazharulislampallab98@gmail.com>
2026-07-01 20:54:35 +0600

    CodeChef Contest problem solve 03

---
+++ Equivalent_Exchange.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int caseTest;
+    cin >> caseTest;
+
+    while (caseTest--) {
+        int N, K;
+        cin >> N >> K;
+
+        int prefix = 0;
+        int mn = 0, mx = 0;
+
+        for (int i = 0; i < N; i++) {
+            int x;
+            cin >> x;
+            prefix += x;
+            mn = min(mn, prefix);
+            mx = max(mx, prefix);
+        }
+
+        if (mx - mn <= K)
+            cout << "Yes\n";
+        else
+            cout << "No\n";
+    }
+
+    return 0;
+}
