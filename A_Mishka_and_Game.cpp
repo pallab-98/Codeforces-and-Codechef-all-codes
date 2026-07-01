41a3a6cc4a0f74de0bfaf42c0552f1a2425cc10b
Pallab <mazharulislampallab98@gmail.com>
2026-07-01 21:47:52 +0600

    Codeforces problem solve 01/07/26

---
+++ A_Mishka_and_Game.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int mishka = 0, chris = 0;
+
+    while (n--)
+    {
+        int m, c;
+        cin >> m >> c;
+
+        if (m > c)
+            mishka++;
+
+        else if (m < c)
+            chris++;
+    }
+
+    if (mishka > chris)
+        cout << "Mishka";
+
+    else if (chris > mishka)
+        cout << "Chris";
+
+    else
+        cout << "Friendship is magic!^^";
+
+    return 0;
+}
