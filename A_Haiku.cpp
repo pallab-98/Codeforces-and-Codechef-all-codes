a447d4bd09109a368c49c7a10b47cc4f28bc3cdb
Pallab <mazharulislampallab98@gmail.com>
2026-07-01 22:17:48 +0600

    Codeforces problem solve 02

---
+++ A_Haiku.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countVowel(string s) {
+    int cnt = 0;
+
+    for (char ch : s) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' ||
+            ch == 'o' || ch == 'u')
+            cnt++;
+    }
+
+    return cnt;
+}
+
+int main() {
+    string s1, s2, s3;
+
+    getline(cin, s1);
+    getline(cin, s2);
+    getline(cin, s3);
+
+    if (countVowel(s1) == 5 &&
+        countVowel(s2) == 7 &&
+        countVowel(s3) == 5)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
